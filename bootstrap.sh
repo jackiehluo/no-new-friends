f2c989e5ac3c45fc9f91c0dace2df5fcc5639fa1
Jackie Luo <jacquelineluo95@gmail.com>
2015-05-12 08:38:18 -0400

    Add Vagrant files and Procfile

---
+++ bootstrap.sh
@@ -0,0 +1,27 @@
+sudo apt-get update
+sudo apt-get install -y python
+sudo apt-get install -y python-dev
+sudo apt-get install -y python-pip
+sudo apt-get install -y vim
+sudo pip install flask
+sudo pip install flask-bcrypt
+sudo pip install flask-debugtoolbar
+sudo pip install flask-login
+sudo pip install flask-mail
+sudo pip install flask-sqlalchemy
+sudo pip install sqlalchemy-migrate
+sudo pip install flask-script
+sudo pip install flask-testing
+sudo pip install flask-wtf
+sudo pip install jinja2
+sudo pip install mako
+sudo pip install markupsafe
+sudo pip install wtforms
+sudo pip install werkzeug
+sudo pip install alembic
+sudo pip install blinker
+sudo pip install coverage
+sudo pip install itsdangerous
+sudo pip install python-bcrypt
+
+echo hello
